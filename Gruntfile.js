e40b0facf176bb0718a228327e0e8b6b64442c57
Linda Nichols <lynnaloo@gmail.com>
2014-02-09 06:41:37 +0000

    add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          // target.css file: source.less file
+          "css/screen.css": "less/screen.less"
+        }
+      }
+    },
+    // Add shell tasks
+    shell: {
+      copyCss: {
+          command: "cp css/screen.css _site/css/screen.css"
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint'],
+      styles: {
+        // Which files to watch
+        files: ['/less/screen.less'],
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks("grunt-shell");
+
+  grunt.registerTask('dev', ['jshint', 'less']);
+
+  grunt.registerTask('default', 'Default Task', function () {
+    grunt.log.writeln('Running jshint and less tasks');
+    grunt.task.run(['jshint', 'less']);
+  });
+};
